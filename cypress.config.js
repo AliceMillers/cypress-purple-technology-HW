0c0020eacc6aa683a3da5fdb304ed0dc4aa9a527
Yulia Bocharova <panterka13@yandex.ru>
2023-01-23 12:23:34 +0100

    Update version of docker image + fix warning

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,6 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   chromeWebSecurity: false,
   fixturesFolder: 'cypress/fixtures',
-  screenshotsFolder: 'cypress/screenshots',
-  videosFolder: 'cypress/videos',
   e2e: {
     baseUrl: 'https://revolgy-forms-case-study-master.staging.axiory.com'
   },
